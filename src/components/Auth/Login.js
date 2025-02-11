dd6537d6165688ccb5acf57d63147e3cc065fe57
JB <drv98@naver.com>
2025-02-11 10:18:52 +0900

    step 삭제

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -15,11 +15,10 @@ import { useEffect } from "react";
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const Login = () => {
-  // Step 1: Login method and Step 2: Verify 2FA
-  const [step, setStep] = useState(1);
+  // 로그인 페이지
   const [jwtToken, setJwtToken] = useState("");
   const [loading, setLoading] = useState(false);
-  // Access the token and setToken function using the useMyContext hook from the ContextProvider
+  // 컨텍스트로 토큰 가져옴
   const { setToken, token } = useMyContext();
   const navigate = useNavigate();
 
@@ -67,11 +66,7 @@ const Login = () => {
       if (response.status === 200 && response.data.jwtToken) {
         setJwtToken(response.data.jwtToken);
         const decodedToken = jwtDecode(response.data.jwtToken);
-        if (decodedToken.is2faEnabled) {
-          setStep(2); // Move to 2FA verification step
-        } else {
-          handleSuccessfulLogin(response.data.jwtToken, decodedToken);
-        }
+        handleSuccessfulLogin(response.data.jwtToken, decodedToken);
       } else {
         toast.error(
           "Login failed. Please check your credentials and try again."
@@ -86,32 +81,6 @@ const Login = () => {
     }
   };
 
-  //function for verify 2fa authentication
-  const onVerify2FaHandler = async (data) => {
-    const code = data.code;
-    setLoading(true);
-
-    try {
-      const formData = new URLSearchParams();
-      formData.append("code", code);
-      formData.append("jwtToken", jwtToken);
-
-      await api.post("/auth/public/verify-2fa-login", formData, {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-        },
-      });
-
-      const decodedToken = jwtDecode(jwtToken);
-      handleSuccessfulLogin(jwtToken, decodedToken);
-    } catch (error) {
-      console.error("2FA verification error", error);
-      toast.error("Invalid 2FA code. Please try again.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
   //if there is token  exist navigate  the user to the home page if he tried to access the login page
   useEffect(() => {
     if (token) navigate("/");
@@ -120,137 +89,93 @@ const Login = () => {
   //step1 will render the login form and step-2 will render the 2fa verification form
   return (
     <div className="min-h-[calc(100vh-74px)] flex justify-center items-center">
-      {step === 1 ? (
-        <React.Fragment>
-          <form
-            onSubmit={handleSubmit(onLoginHandler)}
-            className="sm:w-[450px] w-[360px]  shadow-custom py-8 sm:px-8 px-4"
-          >
-            <div>
-              <h1 className="font-montserrat text-center font-bold text-2xl">
-                Login Here
-              </h1>
-              <p className="text-slate-600 text-center">
-                Please Enter your username and password{" "}
-              </p>
-              <div className="flex items-center justify-between gap-1 py-5 ">
-                <Link
-                  to={`${apiUrl}/oauth2/authorization/google`}
-                  className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
-                >
-                  <span>
-                    <FcGoogle className="text-2xl" />
-                  </span>
-                  <span className="font-semibold sm:text-customText text-xs">
-                    Login with Google
-                  </span>
-                </Link>
-                <Link
-                  to={`${apiUrl}/oauth2/authorization/github`}
-                  className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
-                >
-                  <span>
-                    <FaGithub className="text-2xl" />
-                  </span>
-                  <span className="font-semibold sm:text-customText text-xs">
-                    Login with Github
-                  </span>
-                </Link>
-              </div>
-
-              <Divider className="font-semibold">OR</Divider>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <InputField
-                label="UserName"
-                required
-                id="username"
-                type="text"
-                message="*UserName is required"
-                placeholder="type your username"
-                register={register}
-                errors={errors}
-              />{" "}
-              <InputField
-                label="Password"
-                required
-                id="password"
-                type="password"
-                message="*Password is required"
-                placeholder="type your password"
-                register={register}
-                errors={errors}
-              />
-            </div>
-            <Buttons
-              disabled={loading}
-              onClickhandler={() => {}}
-              className="bg-customRed font-semibold text-white w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3"
-              type="text"
-            >
-              {loading ? <span>Loading...</span> : "LogIn"}
-            </Buttons>
-            <p className=" text-sm text-slate-700 ">
-              <Link
-                className=" underline hover:text-black"
-                to="/forgot-password"
-              >
-                Forgot Password?
-              </Link>
+      <React.Fragment>
+        <form
+          onSubmit={handleSubmit(onLoginHandler)}
+          className="sm:w-[450px] w-[360px]  shadow-custom py-8 sm:px-8 px-4"
+        >
+          <div>
+            <h1 className="font-montserrat text-center font-bold text-2xl">
+              Login Here
+            </h1>
+            <p className="text-slate-600 text-center">
+              Please Enter your username and password{" "}
             </p>
-
-            <p className="text-center text-sm text-slate-700 mt-6">
-              Don't have an account?{" "}
+            <div className="flex items-center justify-between gap-1 py-5 ">
               <Link
-                className="font-semibold underline hover:text-black"
-                to="/signup"
+                to={`${apiUrl}/oauth2/authorization/google`}
+                className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
               >
-                SignUp
+                <span>
+                  <FcGoogle className="text-2xl" />
+                </span>
+                <span className="font-semibold sm:text-customText text-xs">
+                  Login with Google
+                </span>
               </Link>
-            </p>
-          </form>
-        </React.Fragment>
-      ) : (
-        <React.Fragment>
-          <form
-            onSubmit={handleSubmit(onVerify2FaHandler)}
-            className="sm:w-[450px] w-[360px]  shadow-custom py-8 sm:px-8 px-4"
-          >
-            <div>
-              <h1 className="font-montserrat text-center font-bold text-2xl">
-                Verify 2FA
-              </h1>
-              <p className="text-slate-600 text-center">
-                Enter the correct code to complete 2FA Authentication
-              </p>
-
-              <Divider className="font-semibold pb-4"></Divider>
+              <Link
+                to={`${apiUrl}/oauth2/authorization/github`}
+                className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
+              >
+                <span>
+                  <FaGithub className="text-2xl" />
+                </span>
+                <span className="font-semibold sm:text-customText text-xs">
+                  Login with Github
+                </span>
+              </Link>
             </div>
 
-            <div className="flex flex-col gap-2 mt-4">
-              <InputField
-                label="Enter Code"
-                required
-                id="code"
-                type="text"
-                message="*Code is required"
-                placeholder="Enter your 2FA code"
-                register={register}
-                errors={errors}
-              />
-            </div>
-            <Buttons
-              disabled={loading}
-              onClickhandler={() => {}}
-              className="bg-customRed font-semibold text-white w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3"
+            <Divider className="font-semibold">OR</Divider>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <InputField
+              label="UserName"
+              required
+              id="username"
               type="text"
+              message="*UserName is required"
+              placeholder="type your username"
+              register={register}
+              errors={errors}
+            />{" "}
+            <InputField
+              label="Password"
+              required
+              id="password"
+              type="password"
+              message="*Password is required"
+              placeholder="type your password"
+              register={register}
+              errors={errors}
+            />
+          </div>
+          <Buttons
+            disabled={loading}
+            onClickhandler={() => {}}
+            className="bg-customRed font-semibold text-white w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3"
+            type="text"
+          >
+            {loading ? <span>Loading...</span> : "LogIn"}
+          </Buttons>
+          <p className=" text-sm text-slate-700 ">
+            <Link className=" underline hover:text-black" to="/forgot-password">
+              Forgot Password?
+            </Link>
+          </p>
+
+          <p className="text-center text-sm text-slate-700 mt-6">
+            Don't have an account?{" "}
+            <Link
+              className="font-semibold underline hover:text-black"
+              to="/signup"
             >
-              {loading ? <span>Loading...</span> : "Verify 2FA"}
-            </Buttons>
-          </form>
-        </React.Fragment>
-      )}
+              SignUp
+            </Link>
+          </p>
+        </form>
+      </React.Fragment>
     </div>
   );
 };
