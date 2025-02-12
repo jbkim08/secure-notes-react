00d9dd19e68d989d92c0ccc7ca12da01b9aa9688
JB <drv98@naver.com>
2025-02-12 11:21:57 +0900

    주석수정

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -6,12 +6,12 @@ const ProtectedRoute = ({ children, adminPage }) => {
   //컨텍스트로 토큰과 어드민확인 가져옴
   const { token, isAdmin } = useMyContext();
 
-  //1. 토큰이 없으면 로그인으로
+  //1. 토큰이 없으면 로그인으로(인증안됨)
   if (!token) {
     return <Navigate to="/login" />;
   }
 
-  //2. 토큰이 있고 관리자페이고 관리자가 아니면 접속거부
+  //2. 인증된 유저지만 관리자 권한이 없이 관리자 페이지 요청시
   if (token && adminPage && !isAdmin) {
     return <Navigate to="/access-denied" />;
   }
