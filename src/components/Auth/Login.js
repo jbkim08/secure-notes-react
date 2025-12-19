f2ecd3d3b0ac0bd9742dda837075bf5da8a3e85f
KJB <drv98@naver.com>
2025-12-19 09:08:34 +0900

    react fregment <> 삭제

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -81,91 +81,89 @@ const Login = () => {
 
   return (
     <div className="min-h-[calc(100vh-74px)] flex justify-center items-center">
-      <React.Fragment>
-        <form
-          onSubmit={handleSubmit(onLoginHandler)}
-          className="sm:w-[450px] w-[360px]  shadow-custom py-8 sm:px-8 px-4"
-        >
-          <div>
-            <h1 className="font-montserrat text-center font-bold text-3xl">
-              로그인
-            </h1>
+      <form
+        onSubmit={handleSubmit(onLoginHandler)}
+        className="sm:w-[450px] w-[360px]  shadow-custom py-8 sm:px-8 px-4"
+      >
+        <div>
+          <h1 className="font-montserrat text-center font-bold text-3xl">
+            로그인
+          </h1>
 
-            <div className="flex items-center justify-between gap-1 py-5 ">
-              <Link
-                to={`${apiUrl}/oauth2/authorization/google`}
-                className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
-              >
-                <span>
-                  <FcGoogle className="text-2xl" />
-                </span>
-                <span className="font-semibold sm:text-customText text-xs">
-                  Login with Google
-                </span>
-              </Link>
-              <Link
-                to={`${apiUrl}/oauth2/authorization/github`}
-                className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
-              >
-                <span>
-                  <FaGithub className="text-2xl" />
-                </span>
-                <span className="font-semibold sm:text-customText text-xs">
-                  Login with Github
-                </span>
-              </Link>
-            </div>
-
-            <Divider className="font-semibold">OR</Divider>
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <InputField
-              label="UserName"
-              required
-              id="username"
-              type="text"
-              message="유저네임이 필요합니다"
-              placeholder="유저네임 입력..."
-              register={register}
-              errors={errors}
-            />{" "}
-            <InputField
-              label="Password"
-              required
-              id="password"
-              type="password"
-              message="패스워드가 필요합니다"
-              placeholder="패스워드 입력..."
-              register={register}
-              errors={errors}
-            />
-          </div>
-          <Buttons
-            disabled={loading}
-            onClickhandler={() => {}}
-            className="bg-customRed font-semibold text-white w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3"
-            type="text"
-          >
-            {loading ? <span>Loading...</span> : "로그인"}
-          </Buttons>
-          <p className=" text-sm text-slate-700 ">
-            <Link className=" underline hover:text-black" to="/forgot-password">
-              Forgot Password?
-            </Link>
-          </p>
-
-          <p className="text-center text-sm text-slate-700 mt-6">
-            Don't have an account?{" "}
+          <div className="flex items-center justify-between gap-1 py-5 ">
             <Link
-              className="font-semibold underline hover:text-black"
-              to="/signup"
+              to={`${apiUrl}/oauth2/authorization/google`}
+              className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
             >
-              SignUp
+              <span>
+                <FcGoogle className="text-2xl" />
+              </span>
+              <span className="font-semibold sm:text-customText text-xs">
+                Login with Google
+              </span>
             </Link>
-          </p>
-        </form>
-      </React.Fragment>
+            <Link
+              to={`${apiUrl}/oauth2/authorization/github`}
+              className="flex gap-1 items-center justify-center flex-1 border p-2 shadow-sm shadow-slate-200 rounded-md hover:bg-slate-300 transition-all duration-300"
+            >
+              <span>
+                <FaGithub className="text-2xl" />
+              </span>
+              <span className="font-semibold sm:text-customText text-xs">
+                Login with Github
+              </span>
+            </Link>
+          </div>
+
+          <Divider className="font-semibold">OR</Divider>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <InputField
+            label="UserName"
+            required
+            id="username"
+            type="text"
+            message="유저네임이 필요합니다"
+            placeholder="유저네임 입력..."
+            register={register}
+            errors={errors}
+          />{" "}
+          <InputField
+            label="Password"
+            required
+            id="password"
+            type="password"
+            message="패스워드가 필요합니다"
+            placeholder="패스워드 입력..."
+            register={register}
+            errors={errors}
+          />
+        </div>
+        <Buttons
+          disabled={loading}
+          onClickhandler={() => {}}
+          className="bg-customRed font-semibold text-white w-full py-2 hover:text-slate-400 transition-colors duration-100 rounded-sm my-3"
+          type="text"
+        >
+          {loading ? <span>Loading...</span> : "로그인"}
+        </Buttons>
+        <p className=" text-sm text-slate-700 ">
+          <Link className=" underline hover:text-black" to="/forgot-password">
+            Forgot Password?
+          </Link>
+        </p>
+
+        <p className="text-center text-sm text-slate-700 mt-6">
+          Don't have an account?{" "}
+          <Link
+            className="font-semibold underline hover:text-black"
+            to="/signup"
+          >
+            SignUp
+          </Link>
+        </p>
+      </form>
     </div>
   );
 };
