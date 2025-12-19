8922ee9fa1a0be2cb4aa101d1d64b2e4d3572873
KJB <drv98@naver.com>
2025-12-19 10:42:52 +0900

    새 노트 작성하기

--- src/components/Notes/CreateNote.js
+++ src/components/Notes/CreateNote.js
@@ -7,29 +7,30 @@ import api from "../../services/api";
 import Buttons from "../../utils/Buttons";
 import toast from "react-hot-toast";
 
+// 노트 작성 페이지
 const CreateNote = () => {
   const navigate = useNavigate();
-  //set the content of the reactquill
+  //에디터에 적은 내용
   const [editorContent, setEditorContent] = useState("");
   const [loading, setLoading] = useState(false);
-
+  //에디터에 쓴 내용이 바뀌면 바로 업데이트
   const handleChange = (content, delta, source, editor) => {
     setEditorContent(content);
   };
 
-  //note create handler
+  // 노트작성 버튼 클릭시 실행함수
   const handleSubmit = async () => {
     if (editorContent.trim().length === 0) {
-      return toast.error("Note content is required");
+      return toast.error("내용을 적어주세요!"); //종료
     }
     try {
       setLoading(true);
       const noteData = { content: editorContent };
-      await api.post("/notes", noteData);
-      toast.success("Note create successful");
-      navigate("/notes");
+      await api.post("/notes", noteData); //백엔드에 새 노트 작성 요청
+      toast.success("노트 작성 성공!");
+      navigate("/notes"); //내 노트보기 페이지로
     } catch (err) {
-      toast.error("Error creating note");
+      toast.error("노트 작성 에러");
     } finally {
       setLoading(false);
     }
@@ -39,7 +40,7 @@ const CreateNote = () => {
     <div className="min-h-[calc(100vh-74px)] p-10">
       <div className="flex items-center gap-1 pb-5">
         <h1 className="font-montserrat  text-slate-800 sm:text-4xl text-2xl font-semibold ">
-          Create New Note
+          새 노트 작성
         </h1>
         <MdNoteAlt className="text-slate-700 text-4xl" />
       </div>
@@ -75,7 +76,7 @@ const CreateNote = () => {
         onClickhandler={handleSubmit}
         className="bg-customRed  text-white px-4 py-2 hover:text-slate-300 rounded-sm"
       >
-        {loading ? <span>Loading...</span> : " Create Note"}
+        {loading ? <span>Loading...</span> : " 노트 작성"}
       </Buttons>
     </div>
   );
